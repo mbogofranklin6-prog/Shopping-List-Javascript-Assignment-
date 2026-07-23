49df755a8e1b91f8f52a3eff6c5552810c8a66af
franklin <mbogofranklin6@gmail.com>
2026-07-23 15:02:31 +0300

    shopping list manager

---
+++ shopping-list-manager.js
@@ -0,0 +1,8 @@
+let shoppinglist = ["Bread", "Eggs", "Milk", "Yought", "Potatoes"]
+
+function displayitems() {
+    shoppinglist.forEach(function(items) {
+        console.log(items)
+    })
+}
+displayitems();
